afb9dc9e0e271986216eca3f3aa3237fa5a85995
guibrbs <200037994@aluno.unb.br>
2022-05-13 21:24:28 -0300

    :rocket: Adding expand functionality to chat button

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { TouchableOpacity, View } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 import { styles } from './styles';
 import { theme } from '../../theme';
+import BottomSheet from '@gorhom/bottom-sheet/';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
-export function Widget() {
+function Widget() {
+
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  function handleOpen(){
+    bottomSheetRef.current?.expand()
+  }
+
   return (
     <>
      <TouchableOpacity 
       style={styles.button}
+      onPress={handleOpen}
      >
         <ChatTeardropDots 
           size={24}
@@ -16,6 +26,14 @@ export function Widget() {
           color={theme.colors.text_on_brand_color}
         />
       </TouchableOpacity> 
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+      >
+
+      </BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget)
