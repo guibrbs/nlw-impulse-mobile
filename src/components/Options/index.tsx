3a8e12b610c69ddfa08d4b3821a27e79e794bc75
guibrbs <200037994@aluno.unb.br>
2022-05-14 19:49:24 -0300

    :rocket: Adding and styling feedback section

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Copyright } from '../Copyright';
+import React from "react";
+import { View, Text } from "react-native";
+import { Copyright } from "../Copyright";
 
-import { styles } from './styles';
+import { feedbackTypes } from "../../utils/feedbackTypes";
+import { styles } from "./styles";
+import { Option } from "../Option";
 
 export function Options() {
   return (
     <View style={styles.container}>
-        <Copyright />
+      <Text style={styles.title}>
+        Deixe seu feedback
+      </Text>
+      <View style={styles.options}>
+        {Object.entries(feedbackTypes).map(([key, value]) => (
+          <Option key={key} title={value.title} image={value.image} />
+        ))}
+      </View>
+      <Copyright />
     </View>
   );
 }
